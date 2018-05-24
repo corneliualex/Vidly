694ff6dc4b23f0d8b74681dfe6914cc735e98076
pcorneliu <corneliu@rubrikkgroup.com>
2018-05-24 12:31:06 +0300

    Finished ex from tihs section, added validation to Movie

--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using Vidly.Models;
@@ -8,7 +9,49 @@ namespace Vidly.ViewModels
 {
     public class MovieFormViewModel
     {
-        public Movie Movie { get; set; }
+        //public Movie Movie { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
+
+        public int? Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Release date")]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        [Display(Name = "Number in Stock")]
+        public int NumberInStock { get; set; }
+
+        [Required]
+        [Display(Name = "Genres")]
+        public int? GenreId { get; set; }
+
+
+        public string Title
+        {
+            get
+            {
+                return Id != 0 ? "Edit Movie" : "New Movie";
+            }
+        }
+
+        public MovieFormViewModel()
+        {
+            Id = 0;
+        }
+
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            GenreId = movie.GenreId;
+            ReleaseDate = movie.ReleaseDate;
+            NumberInStock = movie.NumberInStock;
+        }
     }
 }
